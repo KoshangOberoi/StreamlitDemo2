68eec52aab3b2422ef86187206f06b245fe8bcf1
KoshangOberoi <koshango@gmail.com>
2024-03-18 11:31:59 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from gaze_tracking import GazeTracking
 import math
 from face_detector import get_face_detector, find_faces
 from face_landmarks import get_landmark_model, detect_marks, draw_marks
+from streamlit_webrtc import webrtc_streamer
+import av
 
 mixer.init()
 sound = mixer.Sound('alarm.wav')
@@ -180,194 +182,199 @@ camera_matrix = np.array(
                          [0, 0, 1]], dtype = "double"
                          )
 
-while(True):
-    ret, frame = cap.read()
-    rects = find_faces(frame, face_model)
-    for rect in rects:
-        shape = detect_marks(frame, landmark_model, rect)
-        draw_marks(frame, shape)
-        cv2.putText(frame, 'Press r to record Mouth distances', (30, 30), font,
-                    1, (0, 255, 255), 2)
-        cv2.imshow("Output", frame)
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-        for i in range(100):
-            for i, (p1, p2) in enumerate(outer_points):
-                d_outer[i] += shape[p2][1] - shape[p1][1]
-            for i, (p1, p2) in enumerate(inner_points):
-                d_inner[i] += shape[p2][1] - shape[p1][1]
-        break
-cv2.destroyAllWindows()
-d_outer[:] = [x / 100 for x in d_outer]
-d_inner[:] = [x / 100 for x in d_inner]
+# while(True):
+#     ret, frame = cap.read()
+#     rects = find_faces(frame, face_model)
+#     for rect in rects:
+#         shape = detect_marks(frame, landmark_model, rect)
+#         draw_marks(frame, shape)
+#         cv2.putText(frame, 'Press r to record Mouth distances', (30, 30), font,
+#                     1, (0, 255, 255), 2)
+#         cv2.imshow("Output", frame)
+#     if cv2.waitKey(1) & 0xFF == ord('r'):
+#         for i in range(100):
+#             for i, (p1, p2) in enumerate(outer_points):
+#                 d_outer[i] += shape[p2][1] - shape[p1][1]
+#             for i, (p1, p2) in enumerate(inner_points):
+#                 d_inner[i] += shape[p2][1] - shape[p1][1]
+#         break
+# cv2.destroyAllWindows()
+# d_outer[:] = [x / 100 for x in d_outer]
+# d_inner[:] = [x / 100 for x in d_inner]
 
-while(True):
-    ret, frame = cap.read()
-    height,width = frame.shape[:2] 
+class VideoProcessor:
+    def recv(self,frame):
+        # ret, frame = cap.read()
+        frm = frame.to_ndarray(format="bgr24")
+        height,width = frm.shape[:2] 
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = find_faces(frame, face_model)
-    # faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
-    left_eye = leye.detectMultiScale(gray)
-    right_eye =  reye.detectMultiScale(gray)
+        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+        faces = find_faces(frm, face_model)
+        # faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
+        left_eye = leye.detectMultiScale(gray)
+        right_eye =  reye.detectMultiScale(gray)
 
-    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+        cv2.rectangle(frm, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
 
-    # for (x,y,w,h) in faces:
-    #     cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
-    rects = find_faces(frame, face_model)
-    for rect in rects:
-        shape = detect_marks(frame, landmark_model, rect)
-        cnt_outer = 0
-        cnt_inner = 0
-        draw_marks(frame, shape[48:])
-        for i, (p1, p2) in enumerate(outer_points):
-            if d_outer[i] + 3 < shape[p2][1] - shape[p1][1]:
-                cnt_outer += 1 
-        for i, (p1, p2) in enumerate(inner_points):
-            if d_inner[i] + 2 <  shape[p2][1] - shape[p1][1]:
-                cnt_inner += 1
-        if cnt_outer > 3 and cnt_inner > 2:
-            print('Mouth open')
-            cv2.putText(frame, 'Mouth open', (30, 30), font,
-                    1, (0, 255, 255), 2)
-    for (x,y,w,h) in right_eye:
-        r_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
-        r_eye = cv2.resize(r_eye,(24,24))
-        r_eye= r_eye/255
-        r_eye=  r_eye.reshape(24,24,-1)
-        r_eye = np.expand_dims(r_eye,axis=0)
-        rpred = np.argmax(model.predict(r_eye),axis=-1)
-        if(rpred[0]==1):
-            lbl='Open' 
-        if(rpred[0]==0):
-            lbl='Closed'
-        break
+        # for (x,y,w,h) in faces:
+        #     cv2.rectangle(frm, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
+        # rects = find_faces(frm, face_model)
+        # for rect in rects:
+        #     shape = detect_marks(frm, landmark_model, rect)
+        #     cnt_outer = 0
+        #     cnt_inner = 0
+        #     draw_marks(frm, shape[48:])
+        #     for i, (p1, p2) in enumerate(outer_points):
+        #         if d_outer[i] + 3 < shape[p2][1] - shape[p1][1]:
+        #             cnt_outer += 1 
+        #     for i, (p1, p2) in enumerate(inner_points):
+        #         if d_inner[i] + 2 <  shape[p2][1] - shape[p1][1]:
+        #             cnt_inner += 1
+        #     if cnt_outer > 3 and cnt_inner > 2:
+        #         print('Mouth open')
+        #         cv2.putText(frm, 'Mouth open', (30, 30), font,
+        #                 1, (0, 255, 255), 2)
+        for (x,y,w,h) in right_eye:
+            r_eye=frm[y:y+h,x:x+w]
+            count=count+1
+            r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+            r_eye = cv2.resize(r_eye,(24,24))
+            r_eye= r_eye/255
+            r_eye=  r_eye.reshape(24,24,-1)
+            r_eye = np.expand_dims(r_eye,axis=0)
+            rpred = np.argmax(model.predict(r_eye),axis=-1)
+            if(rpred[0]==1):
+                lbl='Open' 
+            if(rpred[0]==0):
+                lbl='Closed'
+            break
 
-    for (x,y,w,h) in left_eye:
-        l_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
-        l_eye = cv2.resize(l_eye,(24,24))
-        l_eye= l_eye/255
-        l_eye=l_eye.reshape(24,24,-1)
-        l_eye = np.expand_dims(l_eye,axis=0)
-        lpred = np.argmax(model.predict(l_eye),axis=-1)
-        if(lpred[0]==1):
-            lbl='Open'   
-        if(lpred[0]==0):
-            lbl='Closed'
-        break
+        for (x,y,w,h) in left_eye:
+            l_eye=frm[y:y+h,x:x+w]
+            count=count+1
+            l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+            l_eye = cv2.resize(l_eye,(24,24))
+            l_eye= l_eye/255
+            l_eye=l_eye.reshape(24,24,-1)
+            l_eye = np.expand_dims(l_eye,axis=0)
+            lpred = np.argmax(model.predict(l_eye),axis=-1)
+            if(lpred[0]==1):
+                lbl='Open'   
+            if(lpred[0]==0):
+                lbl='Closed'
+            break
 
-    if(rpred[0]==0 and lpred[0]==0):
-        score=score+1
-        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-    # if(rpred[0]==1 or lpred[0]==1):
-    else:
-        score=score-1
-        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-    
-        
-    if(score<0):
-        score=0   
-    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-    if(score>15):
-        #person is feeling sleepy so we beep the alarm
-        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
-        try:
-            sound.play()
-            
-        except:  # isplaying = False
-            pass
-        if(thicc<16):
-            thicc= thicc+2
+        if(rpred[0]==0 and lpred[0]==0):
+            score=score+1
+            cv2.putText(frm,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        # if(rpred[0]==1 or lpred[0]==1):
         else:
-            thicc=thicc-2
-            if(thicc<2):
-                thicc=2
-        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
-
-    gaze.refresh(frame)
-
-    frame = gaze.annotated_frame()
-    text = ""
-
-    if gaze.is_right():
-        text = "Looking right"
-    elif gaze.is_left():
-        text = "Looking left"
-    elif gaze.is_center():
-        text = "Looking center"
-
-    cv2.putText(frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    faces = find_faces(frame, face_model)
-    for face in faces:
-        marks = detect_marks(frame, landmark_model, face)
-        # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
-        image_points = np.array([
-                                marks[30],     # Nose tip
-                                marks[8],     # Chin
-                                marks[36],     # Left eye left corner
-                                marks[45],     # Right eye right corne
-                                marks[48],     # Left Mouth corner
-                                marks[54]      # Right mouth corner
-                                ], dtype="double")
-        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
-        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
+            score=score-1
+            cv2.putText(frm,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        
             
-            
-            # Project a 3D point (0, 0, 1000.0) onto the image plane.
-            # We use this to draw a line sticking out of the nose
-            
-        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
-            
-        for p in image_points:
-            cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
-            
-            
-            p1 = ( int(image_points[0][0]), int(image_points[0][1]))
-            p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-            x1, x2 = head_pose_points (frame, rotation_vector, translation_vector, camera_matrix)
-
-            cv2.line(frame, p1, p2, (0, 255, 255), 2)
-            cv2.line(frame, tuple(x1), tuple(x2), (255, 255, 0), 2)
-            # for (x, y) in marks:
-            #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
-            # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+        if(score<0):
+            score=0   
+        cv2.putText(frm,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        if(score>5):
+            #person is feeling sleepy so we beep the alarm
+            cv2.imwrite(os.path.join(path,'image.jpg'),frm)
             try:
-                m = (p2[1] - p1[1])/(p2[0] - p1[0])
-                ang1 = int(math.degrees(math.atan(m)))
-            except:
-                ang1 = 90
+                sound.play()
                 
-            try:
-                m = (x2[1] - x1[1])/(x2[0] - x1[0])
-                ang2 = int(math.degrees(math.atan(-1/m)))
-            except:
-                ang2 = 90
+            except:  # isplaying = False
+                pass
+            if(thicc<16):
+                thicc= thicc+2
+            else:
+                thicc=thicc-2
+                if(thicc<2):
+                    thicc=2
+            cv2.rectangle(frm,(0,0),(width,height),(0,0,255),thicc) 
+
+        gaze.refresh(frm)
+
+        frm = gaze.annotated_frame()
+        text = ""
+
+        if gaze.is_right():
+            text = "Looking right"
+        elif gaze.is_left():
+            text = "Looking left"
+        elif gaze.is_center():
+            text = "Looking center"
+
+        cv2.putText(frm, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+        faces = find_faces(frm, face_model)
+        for face in faces:
+            marks = detect_marks(frm, landmark_model, face)
+            # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
+            image_points = np.array([
+                                    marks[30],     # Nose tip
+                                    marks[8],     # Chin
+                                    marks[36],     # Left eye left corner
+                                    marks[45],     # Right eye right corne
+                                    marks[48],     # Left Mouth corner
+                                    marks[54]      # Right mouth corner
+                                    ], dtype="double")
+            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
                 
-                # print('div by zero error')
-            if ang1 >= 48:
-                print('Head down')
-                cv2.putText(frame, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
-            elif ang1 <= -48:
-                print('Head up')
-                cv2.putText(frame, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
-             
-            if ang2 >= 48:
-                print('Head right')
-                cv2.putText(frame, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
-            elif ang2 <= -48:
-                print('Head left')
-                cv2.putText(frame, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
-            
-            cv2.putText(frame, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
-            cv2.putText(frame, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
-    cv2.imshow("Demo", frame)
+                
+                # Project a 3D point (0, 0, 1000.0) onto the image plane.
+                # We use this to draw a line sticking out of the nose
+                
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+                
+            for p in image_points:
+                cv2.circle(frm, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+                
+                
+                p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+                p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+                x1, x2 = head_pose_points (frm, rotation_vector, translation_vector, camera_matrix)
 
-    if cv2.waitKey(1) == 27:
-        break
+                cv2.line(frm, p1, p2, (0, 255, 255), 2)
+                cv2.line(frm, tuple(x1), tuple(x2), (255, 255, 0), 2)
+                # for (x, y) in marks:
+                #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
+                # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+                try:
+                    m = (p2[1] - p1[1])/(p2[0] - p1[0])
+                    ang1 = int(math.degrees(math.atan(m)))
+                except:
+                    ang1 = 90
+                    
+                try:
+                    m = (x2[1] - x1[1])/(x2[0] - x1[0])
+                    ang2 = int(math.degrees(math.atan(-1/m)))
+                except:
+                    ang2 = 90
+                    
+                    # print('div by zero error')
+                if ang1 >= 48:
+                    print('Head down')
+                    cv2.putText(frm, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
+                elif ang1 <= -48:
+                    print('Head up')
+                    cv2.putText(frm, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
+                
+                if ang2 >= 48:
+                    print('Head right')
+                    cv2.putText(frm, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
+                elif ang2 <= -48:
+                    print('Head left')
+                    cv2.putText(frm, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
+                
+                cv2.putText(frm, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
+                cv2.putText(frm, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+        cv2.imshow("Demo", frm)
+        return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
-cap.release()
-cv2.destroyAllWindows()
+        # if cv2.waitKey(1) == 27:
+        #     break
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },sendback_audio=False, video_receiver_size=1)
+# cap.release()
+# cv2.destroyAllWindows()
