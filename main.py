d878775e27968815d73e8ca8de7380c205aeafae
Koshang Oberoi <koshango@gmail.com>
2024-03-19 09:39:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ import av
 # sound = mixer.Sound('alarm.wav')
 
 
-face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
-leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
-reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+face = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+leye = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+reye = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 
 gaze = GazeTracking()
 
